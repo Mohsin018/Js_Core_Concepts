21104e73e6bce7dbb621154e270e83b2fdcbfca8
Mohsin Abdullah Chowdhury <mohsinabd71@gmail.com>
2025-09-02 23:18:17 +0600

    data type complete

---
+++ data-type.js
@@ -0,0 +1,18 @@
+let data = 4;
+data = 'tormuj';
+data = false
+data = {name: 'bangi', price: 40}
+data = [2]
+data = undefined //usually you should not set value to undefined 
+data = null
+console.log(typeof data)
+
+/**
+ * JS is a dynamic typed programming language 
+ * data types are:- string, integer(floot,celi), object, boolean, array
+ * 
+ * 
+ * primitive data type in js:- integer,string,boolean,null,unidentified
+ * 
+ * non primitive data type in js:-  object, array
+*/
